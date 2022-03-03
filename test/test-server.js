1cc69bee2597ea8aed4a40d3067015e68a276077
yanalas1 <yanalas1@gmail.com>
2022-03-03 06:32:11 +0000

    Added documentatioin over the caller service

--- test/test-server.js
+++ test/test-server.js
@@ -1,7 +1,7 @@
 var chai = require('chai');
 const request = require('supertest');
 const app = require('../server');
-
+//specify the url to be intercepted
 describe('GET /', function() {
     it('responds with json', function(done) {
       request(app)
